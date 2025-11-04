6c6aced596e74a31de056ed56621800c2198dc44
Jyoti Prakash Palei <jyotiprakashpalei34@gmail.com>
2025-11-04 12:09:47 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+ 
+// account = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Banglore"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
