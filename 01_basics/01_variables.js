9de6b697e9368119df0596a7b73887830dd31e35
Jyoti Prakash Palei <jyotiprakashpalei34@gmail.com>
2025-11-04 12:33:18 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,5 +12,10 @@ accountCity = "Banglore"
 
 console.log(accountId)
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// Prefer not to use var
+// because of issue in block scope and functional scope
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
 
